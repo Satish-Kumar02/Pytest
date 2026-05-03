1a7166ea1d7781fc05ac5fc317f4592612652079
Satish Kumar <89662867+Satish-Kumar02@users.noreply.github.com>
2026-05-03 17:04:17 +0530

    adding nav list

--- tutorial_ninja/components/navbar.py
+++ tutorial_ninja/components/navbar.py
@@ -5,8 +5,14 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class navbar(basepage):
     _navbar= (By.XPATH,"//nav[@id='menu']")
+    _navbar_menu= (By.CSS_SELECTOR, "ul.nav.navbar-nav > li")
     # def get_navbar(self):
     #     self.type(self._navbar)
     
     def is_displayed(self):
         return self.is_visible(self._navbar)
+
+    def get_menu(self):
+        menu_items = self.find_elements(*self._navbar_menu)
+        print("Debug elements->", menu_items)
+        return [el.text.strip()for el in menu_items]
--- tutorial_ninja/tests/test_navbar.py
+++ tutorial_ninja/tests/test_navbar.py
@@ -1,12 +1,19 @@
 import pytest
 import allure
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from page_objects.home_page import home_page
+from components.navbar import navbar
 from selenium.webdriver.support import expected_conditions as EC
 from unittest import result
 
-@pytest.mark.navbar
+#@pytest.mark.navbar
 def test_navbar_visible(browser):
     home = home_page(browser)
     assert home.navbar.is_displayed()
+
+@pytest.mark.navbar    
+def test_menu(browser):
+    lists= ['Desktops', 'Laptops & Notebooks', 'Components', 'Tablets', 'Software', 'Phones & PDAs', 'Cameras', 'MP3 Players']
+    home = home_page(browser)
+    menus = home.navbar.get_menu()
+    assert lists == menus
